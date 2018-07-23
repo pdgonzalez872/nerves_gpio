31d8da0bbfa5a8b5613c999335531356d061d677
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-07-23 18:45:05 -0500

    Adjust code to not always run

--- lib/nerves_basic_skeleton.ex
+++ lib/nerves_basic_skeleton.ex
@@ -16,31 +16,29 @@ defmodule NervesBasicSkeleton do
     :yay
   end
 
-  ## Copy paste to check things out
+  ## This is Frank's code
 
   require Logger
 
   alias ElixirALE.GPIO
 
   @output_pin Application.get_env(:hello_gpio, :output_pin, 26)
-  @input_pin Application.get_env(:hello_gpio, :input_pin, 20)
   @sleep_time(1000)
 
-  def start(_type, _args) do
+  def new_led_pid() do
     Logger.info("Starting pin #{@output_pin} as output")
     {:ok, output_pid} = GPIO.start_link(@output_pin, :output)
-    spawn(fn -> toggle_pin_forever(output_pid) end)
+    #spawn(fn -> toggle_pin_forever(output_pid) end)
+    output_pid
   end
 
-  defp toggle_pin_forever(output_pid) do
+  def turn_on(pid) do
     Logger.debug("Turning pin #{@output_pin} ON")
     GPIO.write(output_pid, 1)
-    Process.sleep(@sleep_time)
+  end
 
+  def turn_off(pid) do
     Logger.debug("Turning pin #{@output_pin} OFF")
     GPIO.write(output_pid, 0)
-    Process.sleep(@sleep_time)
-
-    toggle_pin_forever(output_pid)
   end
 end
