70c1e24db3e1bd6611dee25abe0e1147fb326712
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-07-23 17:05:04 -0500

    Add config

--- config/config.exs
+++ config/config.exs
@@ -1,23 +1,34 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# Customize non-Elixir parts of the firmware.  See
-# https://hexdocs.pm/nerves/advanced-configuration.html for details.
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
-# Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
 config :shoehorn,
   init: [:nerves_runtime],
   app: Mix.Project.config()[:app]
 
-# Import target specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-# Uncomment to use target specific configurations
+config :logger, backends: [RingLogger]
 
-# import_config "#{Mix.Project.config[:target]}.exs"
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!, ".ssh/id_rsa.pub"))
+  ]
+
+config :nerves_basic_skeleton, interface: :wlan0, port: 4001
+
+config :nerves_init_gadget,
+  node_name: :gpio,
+  mdns_domain: "nerves_gpio.local",
+  address_method: :dhcp,
+  ifname: "wlan0"
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
