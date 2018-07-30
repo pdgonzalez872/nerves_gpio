7c7f02db11ff91fb50ced9086cb0f50958dd7f29
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-07-30 10:40:06 -0500

    Adjust in between to turn on and off

--- in_between/lib/in_between.ex
+++ in_between/lib/in_between.ex
@@ -1,18 +1,27 @@
 defmodule InBetween do
-  @moduledoc """
-  Documentation for InBetween.
-  """
+  ## This is Frank's code
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  alias ElixirALE.GPIO
 
-      iex> InBetween.hello
-      :world
+  @output_pin(26)
+  @sleep_time(1000)
 
-  """
-  def hello do
-    :world
+  def new_led_pid() do
+    Logger.info("Starting pin #{@output_pin} as output")
+    {:ok, output_pid} = GPIO.start_link(@output_pin, :output)
+    #spawn(fn -> toggle_pin_forever(output_pid) end)
+    output_pid
+  end
+
+  def turn_on(pid) do
+    Logger.debug("Turning pin #{@output_pin} ON")
+    GPIO.write(pid, 1)
+  end
+
+  def turn_off(pid) do
+    Logger.debug("Turning pin #{@output_pin} OFF")
+    GPIO.write(pid, 0)
   end
 end
